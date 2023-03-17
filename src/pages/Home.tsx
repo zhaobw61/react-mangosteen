ba6673f2bce1ce80bfe9206d42ff318033181614
zhaobowen <1964471757@qq.com>
2023-03-17 16:35:41 +0800

    fix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,28 @@
+import useSWR from 'swr'
+import { Navigate } from 'react-router-dom'
 import p from '../assets/images/pig.svg'
 import add from '../assets/icons/add.svg'
+import { ajax } from '../lib/ajax'
 import styles from './Home.module.scss'
 
 export const Home: React.FC = () => {
+  const { data: meData, error: meError } = useSWR('/api/v1/me', async (path) => {
+    const response = await ajax.get<Resource<User>>(path)
+    return response.data.resource
+  })
+  const { data: itemsData, error: itemsError } = useSWR('/api/v1/items', async (path) => {
+    const response = await ajax.get<Resources<Item>>(path)
+    return response.data
+  })
+  const isLoadingMe = !meData && !meError
+  const isLoadingItems = meData && !itemsData && !itemsError
+
+  if (isLoadingMe || isLoadingItems)
+    return <div>加载中……</div>
+
+  if (itemsData?.resources[0])
+    return <Navigate to="/items" />
+
   return (
     <div>
       <div className={styles.logoWrap}>
