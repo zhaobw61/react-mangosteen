16a1e0e4b1e48cdd48e10ce12d8d97d00e5fd494
zhaobowen <1964471757@qq.com>
2023-03-24 17:07:51 +0800

    init time ranger picker

--- src/components/TimeRangerPicker.tsx
+++ src/components/TimeRangerPicker.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import styles from './TimeRangerPicker.module.scss'
 
 export const TimeRangerPicker: React.FC = () => {
   return (
-    <div>TimeRangerPicker</div>
+    <div className={styles.timeRangerPickerWrap}>
+      <ol>
+        <li>本月</li>
+        <li>上个月</li>
+        <li>今年</li>
+        <li>自定义时间</li>
+      </ol>
+    </div>
   )
 }
--- src/components/Topnav.tsx
+++ src/components/Topnav.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import listSVG from '../assets/icons/list.svg'
+import styles from './Topnav.module.scss'
 
 export const Topnav: React.FC = () => {
   return (
-    <div>Topnav</div>
+    <div className={styles.topNavWrap}>
+      <img src={listSVG} alt="" />
+      <h1>TRON 记账</h1>
+    </div>
   )
 }
