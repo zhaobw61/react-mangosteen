9679ad860fbace6d2a38b80194369f60daf6f4cf
zhaobowen <1964471757@qq.com>
2023-04-22 17:08:20 +0800

    fixfix

--- src/pages/ItemPage.tsx
+++ src/pages/ItemPage.tsx
@@ -17,31 +17,6 @@ import styles from './ItemPage.module.scss'
 
 export const ItemPage: React.FC = () => {
   const [timeRange, setTimeRange] = useState<TimeRange>('thisMonth')
-  const [items, setItems] = useState<Item[]>([
-    {
-      id: 13,
-      user_id: 123,
-      amount: 123,
-      note: 'dq1e',
-      tag_ids: [1],
-      happen_at: '123123',
-      created_at: '12asd213',
-      updated_at: 'a12312',
-      kind: 'expenses',
-      deleted_at: 'string'
-    }, {
-      id: 33311233,
-      user_id: 31666112323,
-      amount: 777123,
-      note: 'gggdq1e',
-      tag_ids: [1],
-      happen_at: 'g1g23123',
-      created_at: '1g212asd213',
-      updated_at: 'a2312312',
-      kind: 'expenses',
-      deleted_at: 's123123tring'
-    }
-  ])
   // const [visible, setVisible] = useState(false)
   const { visible, setVisible } = useMenuStore()
   return (
@@ -55,7 +30,7 @@ export const ItemPage: React.FC = () => {
           <TimeRangerPicker selected={timeRange} onSelected={setTimeRange} />
         </div>
         <ItemsSummary />
-        <ItemsList items={items} />
+        <ItemsList />
         <AddItemFloatButton />
         { visible
           ? <TopMenu onClickMask={() => {
--- src/pages/ItemsPage/ItemsList.tsx
+++ src/pages/ItemsPage/ItemsList.tsx
@@ -1,11 +1,46 @@
+import useSWR from 'swr'
 import React from 'react'
+import { ajax } from '../../lib/ajax'
 import styles from './ItemsList.module.scss'
 
 interface Props {
-  items: Item[]
 }
 
-export const ItemsList: React.FC<Props> = ({ items }) => {
+export const ItemsList: React.FC<Props> = () => {
+  const pageIndex = 0
+  const { items1, error } = useSWR(`/api/v1/items?page=${pageIndex + 1}`, async (path) => {
+    const response = await ajax.get<Resources<Item>>(path)
+    console.log('response', response.data)
+    return response.data
+  })
+  const items: [Item] = [
+    {
+      id: 13,
+      user_id: 123,
+      amount: 123,
+      note: 'dq1e',
+      tag_ids: [1],
+      happen_at: '123123',
+      created_at: '12asd213',
+      updated_at: 'a12312',
+      kind: 'expenses',
+      deleted_at: 'string'
+    }, {
+      id: 33311233,
+      user_id: 31666112323,
+      amount: 777123,
+      note: 'gggdq1e',
+      tag_ids: [1],
+      happen_at: 'g1g23123',
+      created_at: '1g212asd213',
+      updated_at: 'a2312312',
+      kind: 'expenses',
+      deleted_at: 's123123tring'
+    }
+  ]
+  const onLoadMore = () => {
+    console.log('onLoadMore')
+  }
   return (
     <div className={styles.ItemsList}>
       <ol>
@@ -23,7 +58,7 @@ export const ItemsList: React.FC<Props> = ({ items }) => {
         </li>)}
       </ol>
       <div className='buttonWrap'>
-        <button className='button'>加载更多</button>
+        <button className='button' onClick={onLoadMore}>加载更多</button>
       </div>
     </div>
   )
